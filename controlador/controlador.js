625d92be49f845fece2e0770b968105f202825ea
Camilo <titanasura@gmail.com>
2024-05-01 19:21:06 -0500

    commit4

--- controlador/controlador.js
+++ controlador/controlador.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', function (event) {
     event.preventDefault();
 
     if (whatsappRadio.checked) {
-        const phoneNumber = '573114909348';
+        const phoneNumber = '573205254347';
         const nombreApellido = document.getElementById("nombreapellido").value;
         const franjaHoraria = document.getElementById("FranjaHoraria").value; 
         const mensajeCuadro = document.getElementById ("mensaje").value;
@@ -21,7 +21,7 @@ form.addEventListener('submit', function (event) {
         window.location.href = `https://wa.me/${phoneNumber}?text=Hola mi nombre es: ${nombreApellido}, mi número de WhatsApp es: ${celular}, me comunico por el siguiente motivo: ${mensajeCuadro}. Pueden contactarme en horas de la ${franjaHoraria}. Gracias por su atención.`;
     }
     else if (radioemail.checked) {
-        const phoneNumber = '573114909348';
+        const phoneNumber = '573205254347';
         const nombreApellido = document.getElementById("nombreapellido").value;
         const franjaHoraria = document.getElementById("FranjaHoraria").value; 
         const mensajeCuadro = document.getElementById ("mensaje").value;
@@ -30,7 +30,7 @@ form.addEventListener('submit', function (event) {
         window.location.href = `https://wa.me/${phoneNumber}?text=Hola mi nombre es: ${nombreApellido}, mi número de telefono es: ${celular}, me comunico por el siguiente motivo: ${mensajeCuadro}. Pueden contactarme en horas de la ${franjaHoraria}, mi correo es: ${correo}. Gracias por su atención.`;
     }
     else if (radiotelefono.checked){
-        const phoneNumber='573114909348';
+        const phoneNumber='573205254347';
         const nombreApellido=document.getElementById("nombreapellido").value;
         const franjaHoraria=document.getElementById("FranjaHoraria").value; 
         const mensajeCuadro=document.getElementById ("mensaje").value;
